bfdf93bb79421d766e28b2cda100cb4bc88e1d29
Iskander <iskandar.13.aburahmonov@gmail.com>
2025-10-23 16:05:20 +0500

    18. meetings data table

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -4,6 +4,9 @@ import {useTRPC} from "@/trpc/client";
 import { useSuspenseQuery} from "@tanstack/react-query";
 import {LoadingState} from "@/components/loading-state";
 import {ErrorState} from "@/components/error-state";
+import {DataTable} from "@/components/data-table";
+import {columns} from "@/modules/meetings/ui/components/columns";
+import {EmptyState} from "@/components/empty-state";
 
 export const MeetingsView = () => {
     const trpc = useTRPC()
@@ -11,8 +14,14 @@ export const MeetingsView = () => {
     const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
 
     return (
-        <div>
-            {JSON.stringify(data)}
+        <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+            <DataTable columns={columns} data={data.items}/>
+            {data.items.length === 0 && (
+                <EmptyState
+                    title="Create your first meeting"
+                    description="Schedule a meeting to connect with others. Each meeting lets you collaborate, share ideas, and interact with participants in real time."
+                />
+            )}
         </div>
     )
 }
